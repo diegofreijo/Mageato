b93a32a0194679a589c6f57f2c094e7741ff8c07
Diego <giga.freijo@gmail.com>
2023-11-16 17:57:32 -0300

    clase 4-2

---
+++ Assets/Code/SqlExample/CreateDBScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateDBScript : MonoBehaviour
+{
+    public Text DebugText;
+
+    // Use this for initialization
+    void Start()
+    {
+        var ds = new DataService("tempDatabase.db");
+        ds.CreateDB();
+
+        ds.ActualizaPrimerNombre("Pepe");
+
+        var personas = ds.TodasLasPersonas();
+        ToConsole(personas);
+
+        var nombre = ds.NombreDelPrimero();
+        ToConsole(nombre);
+
+        // var people = ds.GetPersons();
+        // ToConsole(people);
+
+        // people = ds.GetPersonsNamedRoberto();
+        // ToConsole("Searching for Roberto ...");
+        // ToConsole(people);
+    }
+
+    private void ToConsole(IEnumerable<Person> people)
+    {
+        foreach (var person in people)
+        {
+            ToConsole(person.ToString());
+        }
+    }
+
+    private void ToConsole(string msg)
+    {
+        DebugText.text += System.Environment.NewLine + msg;
+        Debug.Log(msg);
+    }
+}
---
+++ Assets/Code/SqlTest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SqlTest : MonoBehaviour
+{
+    void Start()
+    {
+        // var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "MyData.db");
+        // var db = new SQLiteConnection(databasePath);
+    }
+
+}
